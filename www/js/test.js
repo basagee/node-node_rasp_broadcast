d439edb06b7652311593daf9165ca522281a3dfc
basagee <basagee@gmail.com>
2017-01-09 12:10:40 +0900

    user media test code..

--- www/js/test.js
+++ www/js/test.js
@@ -5,7 +5,28 @@
 /*
  * real initialize
  */
-window.onload = function () {
+ window.nativeRTCPeerConnection = (window.webkitPeerConnection00 || window.webkitRTCPeerConnection || window.mozRTCPeerConnection);
+ window.nativeRTCSessionDescription = (window.mozRTCSessionDescription || window.RTCSessionDescription); // order is very important: "RTCSessionDescription" defined in Nighly but useless
+ window.nativeRTCIceCandidate = (window.mozRTCIceCandidate || window.RTCIceCandidate);
+ window.nativeURL = (window.webkitURL || window.URL);
+ navigator.nativeGetUserMedia = (navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+ var o_stream = null;
+ var o_sdp;
+ var o_pc;
+ var o_offer, o_answer;
+ var b_remote_sdp = false;
+ var b_firefox = false;
+ var o_webkitGetUserMediaHasType = { audio: true, video: true };
+ var o_MediaContraintsHasType = { audio: true, video: true };
+ var o_media_constraints =
+ { 'mandatory':
+     {
+         'OfferToReceiveAudio': o_MediaContraintsHasType.audio,
+         'OfferToReceiveVideo': o_MediaContraintsHasType.video
+     }
+ };
+
+ window.onload = function () {
     if (typeof history.pushState === "function") {
         history.pushState("nbp-iotgw", null, null);
         window.onpopstate = function () {
@@ -35,6 +56,18 @@ window.onload = function () {
     }
     initJavascriptBridge();
     document.getElementById("btn").addEventListener("click", onBackPressed);
+    window.console.info("window.onload");
+    if (navigator.nativeGetUserMedia) {
+        navigator.nativeGetUserMedia({ audio: o_webkitGetUserMediaHasType.audio, video: o_webkitGetUserMediaHasType.video },
+        function (stream) {
+            o_stream = stream;
+            // document.getElementById("btnTestOffer").disabled = false;
+            // document.getElementById("btnTestAnswer").disabled = false;
+        },
+        function (e) {
+            console.error(e.toString());
+        });
+    }
 }
 
 function onBackPressed() {
